5b381ff357bd9394c6b935dbc54247d0929e8a9e
phamhoangtung95-create <phamhoangtung95@gmail.com>
2026-09-14 22:56:30 +0700

    fix: enforce absolute API endpoint for sequence emails and add CORS headers

--- api/send-sequence.js
+++ api/send-sequence.js
@@ -173,7 +173,7 @@ async function sendOneEmail(apiKey, fromEmail, to, emailData) {
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   if (req.method !== 'POST') {
